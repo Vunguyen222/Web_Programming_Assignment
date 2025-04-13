9c8fdb9aefba5cf72e772dab3a231f385b007f40
Vunguyen222 <vunguyenking@gmail.com>
2025-04-13 17:08:25 +0700

    reAdd folder

---
+++ BTL_WEB/front_end/src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import bitis_logo from '../assets/bitis_logo.svg'
+import { Link } from "react-router-dom";
+export default function Logo({ childClass }) {
+  return (
+    <Link to="/" className={`${childClass}`}>
+      <img src={bitis_logo} alt="Logo"/>
+    </Link>
+  )
+}
